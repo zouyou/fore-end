c60db403dc4de3adbf4309a12da1a6f941f96e49
邹世友 <zoushy@yonyou.com>
2018-06-02 15:09:49 +0800

    1

--- src/views/home/Login.vue
+++ src/views/home/Login.vue
@@ -1,5 +1,45 @@
 <template>
-    <div >
-欢迎登录！
-    </div>
+<div>
+<el-form label-position="right" :model="ruleForm" :rules="rules" ref="ruleForm" size="mini" label-width="100px">
+  <el-form-item label="账号" prop="username">
+    <el-input  v-model="ruleForm.username" auto-complete="off"></el-input>
+  </el-form-item>
+  <el-form-item label="密码" prop="password">
+    <el-input type="password" v-model="ruleForm.password" auto-complete="off"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitForm()">提交</el-button>
+    <el-button @click="resetForm()">重置</el-button>
+  </el-form-item>
+</el-form>
+</div>
 </template>
+<script>
+export default {
+  data: function() {
+    return {
+      ruleForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [{ required: true, message: "请输入账号" }],
+        password: [{ required: true, message: "请输入密码" }]
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      const self = this;
+      self.$refs.ruleForm.validate(valid => {
+        if (valid) {
+          self.$message("验证成功！");
+        }
+      });
+    },
+    resetForm() {
+      this.$refs.ruleForm.resetFields();
+    }
+  }
+};
+</script>
